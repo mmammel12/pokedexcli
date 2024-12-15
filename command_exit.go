aaeb1a0ed22f40418f9afce71035de356ae00a07
Marty Mammel <mmammel12@gmail.com>
2024-12-15 08:27:07 -0600

    complete new lessons

--- command_exit.go
+++ command_exit.go
@@ -7,6 +7,7 @@ import (
 )
 
 func commandExit(*config, []string) error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 
 	return fmt.Errorf("Failed to exit")
--- command_help.go
+++ command_help.go
@@ -3,7 +3,6 @@ package main
 import "fmt"
 
 func commandHelp(*config, []string) error {
-	fmt.Println()
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
 	fmt.Println()
